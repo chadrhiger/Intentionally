430f8c0486f8c484d6230cada3cec676c1e91838
Chad Rhiger <csrhig@gmail.com>
2022-05-23 16:12:04 -0500

    Intial commit. Added to database.sql

--- database.sql
+++ database.sql
@@ -8,3 +8,46 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "user" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL
+);
+
+select * from "user";
+
+create table "goals" (
+	"id" SERIAL PRIMARY KEY,
+	"text" varchar (1000) UNIQUE NOT NULL,
+	"user_id" int,
+	 FOREIGN KEY ("user_id") references "user"("id")
+);
+
+ALTER TABLE "goals" ADD COLUMN 
+"inserted_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+
+create table "answers" (
+	"id" SERIAL PRIMARY KEY,
+	"goal_met" boolean,
+	"goal_id" int,
+	 FOREIGN KEY ("goal_id") references "goals"("id")
+);
+
+ALTER TABLE "answers" ADD COLUMN 
+"inserted_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+
+select * from "answers";
+
+select * from "goals";
+
+select * from "user";
+
+
+
+
+
+
+
