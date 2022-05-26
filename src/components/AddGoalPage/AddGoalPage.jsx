89bb0cf80f50e10d69d6331b18d6f60c66a9d773
Chad Rhiger <csrhig@gmail.com>
2022-05-26 13:58:12 -0500

    Added button that brings user back to GoalsPage

--- src/components/AddGoalPage/AddGoalPage.jsx
+++ src/components/AddGoalPage/AddGoalPage.jsx
@@ -5,13 +5,31 @@ import { useHistory } from 'react-router-dom';
 
 function AddGoalsPage() {
   console.log('in AddGoalsPage()');
- 
 
+  const history = useHistory();
+
+
+
+  const handleGoalsClick = () => {
+    // dispatch({ 
+    //   type: 'FETCH_MOVIE',
+    //   payload: movie.id
+    // });
+    // dispatch({
+    //   type: 'FETCH_GENRE',
+    //   payload: movie.id
+    // });
+    // console.log('genre id:', movie.id);
+    history.push('/goals');
+  }
 
   return (
-    <p>Add Goals on this Page Someday!!</p>
+    <>
+      <p>Add Goals on this Page Someday!!</p>
+      <button onClick={handleGoalsClick}>Back to Goals</button>
+    </>
   );
-   
+
 
 }
 
